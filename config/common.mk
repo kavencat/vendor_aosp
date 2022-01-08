4d9402b1b75a7e1e8afaf0aa0b809e1474b39295
SagarMakhar <sagarmakhar@gmail.com>
2022-01-08 18:49:07 +0000

    vendor: Black theme [3/3]
    Change-Id: I362c75a9a9456bb4d7ffe12072b1b2ef1ea49dac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
     Author:    SagarMakhar <sagarmakhar@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -103,6 +103,10 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common
 
+# Themes
+PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay
+
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
