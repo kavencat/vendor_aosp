13b81688c54213996f31968d0de3650004284bc4
Techy0302 <99090768+Techy0302@users.noreply.github.com>
2022-09-16 23:52:04 +0530

    Bacon: Fix build file name

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CUSTOM_VERSION).zip
+CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(ETERNITY_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
